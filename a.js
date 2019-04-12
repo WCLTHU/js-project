e25f6aa4de6317a451a658da38c4a2ec78431c5a
WCLTHU <43700479+WCLTHU@users.noreply.github.com>
2019-04-12 09:47:44 +0800

    这两个是外部引用js代码的练习

---
+++ a.js
@@ -0,0 +1,6 @@
+  function init(){
+    var Divsign=document.getElementById('agou')
+      Divsign.innerHTML=''
+      Divsign.setAttribute('style','height:150px;width:500px;background-color:green;position: relative;top:20px;left:10px')
+  }
+  window.onload=init
